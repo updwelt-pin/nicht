f85d2eb4e394779dde4f8a379896791879dc6ffc
updwelt-pin <updwelt-pin@outlook.com>
2023-08-17 05:31:36 +0800

    (ADD) common.h

---
+++ pa/common.h
@@ -0,0 +1,10 @@
+#ifndef _h_common_updwelt_pin_des_aux
+#define _h_common_updwelt_pin_des_aux
+#define noresult void
+#include<stdint.h>
+#include<stdio.h>
+#include<stdlib.h>
+typedef void* any_pa;
+typedef uint32_t idx_pa;
+typedef uint8_t u8_pa;
+#endif
