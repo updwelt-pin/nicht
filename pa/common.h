f2b2ccf05b1f06bbf0adfa096faf79426960360d
Updwelt Pins <141853152+updwelt-pin@users.noreply.github.com>
2023-08-24 00:18:28 +0800

    (RM) common.h

--- pa/common.h
+++
@@ -1,9 +0,0 @@
-#ifndef _h_common_updwelt_pin_des_aux
-#define _h_common_updwelt_pin_des_aux
-#define noresult void
-#include <stdint.h>
-#include <stdio.h>
-#include <stdlib.h>
-typedef void* Any_pa;
-typedef uint32_t Idx_pa;
-#endif
